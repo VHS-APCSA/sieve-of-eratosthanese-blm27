46efc3d8b8a67275da9be2957a20c44910b9e2bd
littl <littl@DESKTOP-IK0GM3J.attlocal.net>
2017-12-17 12:25:00 -0800

    Fixed Sieve class.

--- src/Sieve.java
+++ src/Sieve.java
@@ -15,22 +15,18 @@ public class Sieve
 		}
 	}
 	public void findprimeNums()
-	{
-		for(int i = 2; i < array.length; i++)
 		{
-			if(array[i])
+			for(int i = 2; i*i < array.length; i++)
 			{
-				for(int i2 = 2; i2 < array.length; i2++)
+				if(array[i])
 				{
-					array[i] = true;
-					if(i2%i ==0)
+					for(int i2 = i*2; i2 < array.length; i2 += i)
 					{
 						array[i2] = false;
 					}
 				}
 			}
 		}
-	}
 	public String toString()
 	{
 		String ret = "{";
